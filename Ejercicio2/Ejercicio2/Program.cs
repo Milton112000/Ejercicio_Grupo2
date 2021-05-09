eee6c1db4654e78cbf37caf1b8c1f05d8612b55c
Milton Orellana <go0211032020@unab.edu.sv>
2021-05-09 11:25:35 -0600

    ejercicio 2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String Nombre;
+            int edad;
+            String Genero;
+
+            Console.WriteLine("Introduzca su Nombre");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Introduzca su Edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduzca su Genero una (M) si es Hombre o una (F) si es Mujer ");
+            Genero = Console.ReadLine();
+
+
+            switch (Genero)
+            {
+                case "f" : Console.WriteLine("La Señora: "+Nombre+ " Se a Registrado");
+                    break;
+
+                case "F":
+                    Console.WriteLine("La Señora: " + Nombre + " Se a Registrado");
+                    break;
+
+                case "m":
+                    Console.WriteLine("El Señor: " + Nombre + " Se a Registrado");
+                    break;
+
+                case "M":
+                    Console.WriteLine("El Señor: " + Nombre + " Se a Registrado");
+                    break;
+
+                default: Console.WriteLine("error en Datos Ingresados en Campo Genero");
+                    break;
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
